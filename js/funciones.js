7c6aa59c669d8d913fbd6ee9da7299b9e2490729
Julian Riedinger <julianriedinger27@gmail.com>
2026-04-17 11:14:04 -0300

    feat: funciones comunes de localStorage y calculo total

---
+++ js/funciones.js
@@ -0,0 +1,48 @@
+// Se pasa objeto gasto con los siguientes campos: { nombre, categoria, monto, fecha }
+function guardarGasto(gasto) {
+  const gastos = JSON.parse(localStorage.getItem("gastos")) || [];
+  const esta = gastos.some(
+    (g) =>
+      g.nombre === gasto.nombre &&
+      g.categoria === gasto.categoria &&
+      g.monto === gasto.monto &&
+      g.fecha === gasto.fecha,
+  );
+  if (!esta) {
+    gastos.push(gasto);
+    localStorage.setItem("gastos", JSON.stringify(gastos));
+  }
+}
+
+function obtenerGastos() {
+  return JSON.parse(localStorage.getItem("gastos")) || [];
+}
+
+function guardarCategoria(categoria) {
+  const categorias = JSON.parse(localStorage.getItem("categorias")) || [];
+  if (!categorias.includes(categoria)) {
+    categorias.push(categoria);
+    localStorage.setItem("categorias", JSON.stringify(categorias));
+  }
+}
+
+function obtenerCategorias() {
+  return JSON.parse(localStorage.getItem("categorias")) || [];
+}
+
+function eliminarGasto(gasto) {
+  const gastos = JSON.parse(localStorage.getItem("gastos")) || [];
+  const gastosFiltrados = gastos.filter(
+    (g) =>
+      g.nombre !== gasto.nombre ||
+      g.categoria !== gasto.categoria ||
+      g.monto !== gasto.monto ||
+      g.fecha !== gasto.fecha,
+  );
+  localStorage.setItem("gastos", JSON.stringify(gastosFiltrados));
+}
+
+// Recibe un array de objetos gasto y devuelve el total de los montos
+function calcularTotalGastos(gastos) {
+  return gastos.reduce((total, gasto) => (total += gasto.monto), 0);
+}
