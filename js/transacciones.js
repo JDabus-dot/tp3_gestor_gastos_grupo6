a6a17649de53e0ea58cce9d0a22d4ffc8cb66428
Julian Riedinger <julianriedinger27@gmail.com>
2026-04-17 18:56:48 -0300

    feat: estilado de registro de transacciones

--- js/transacciones.js
+++ js/transacciones.js
@@ -5,7 +5,12 @@ const listaGastos = $("#lista-gastos");
 $(document).ready(function () {
   const gastos = obtenerGastos();
   gastos.forEach((gasto) => {
-    const gastoItem = `<li>${gasto.nombre} - ${gasto.categoria} - $${gasto.monto} - ${gasto.fecha}</li>`;
+    const gastoItem = `<li>
+    <span>${gasto.nombre}</span>
+    <span>${gasto.categoria}</span>
+    <span>$${gasto.monto}</span>
+    <span>${gasto.fecha}</span>
+    </li>`;
     listaGastos.append(gastoItem);
   });
 });
