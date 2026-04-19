30ba7dcbd283839b219fbf08c6a26ba9198b0e3c
Julian Riedinger <julianriedinger27@gmail.com>
2026-04-19 11:54:37 -0300

    feat: se agrego boton eliminar y estilos responsive

--- js/transacciones.js
+++ js/transacciones.js
@@ -5,12 +5,14 @@ const listaGastos = $("#lista-gastos");
 $(document).ready(function () {
   const gastos = obtenerGastos();
   gastos.forEach((gasto) => {
-    const gastoItem = `<li>
-    <span>${gasto.nombre}</span>
-    <span>${gasto.categoria}</span>
-    <span>$${gasto.monto}</span>
-    <span>${gasto.fecha}</span>
-    </li>`;
+    const gastoItem = `
+        <li>
+          <span>${gasto.nombre}</span>
+          <span>${gasto.categoria}</span>
+          <span>$${gasto.monto}</span>
+          <span>${gasto.fecha}</span>
+          <button class="btn">❌</button>
+        </li>`;
     listaGastos.append(gastoItem);
   });
 });
